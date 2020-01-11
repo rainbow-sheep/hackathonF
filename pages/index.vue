cf3db57dd4d0cd945005d4b74442d285cd8e98e8
Shou C <scf@ieee.org>
2020-01-11 15:20:57 -0800

    add more files

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="background">
+      <Page>
+        <Nav></Nav>
+      </Page>
+    </div>
+  </div>
+
+
+
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Nav from "../components/Nav";
+import Page from "../components/Page";
+
+export default {
+  components: {
+    Page,
+    Nav,
+    Logo
+  }
+}
+</script>
+
+<style scoped>
+  .background{
+
+  }
+</style>
---
+++ plugins/element-ui.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Element from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+
+Vue.use(Element, { locale })
+import VueTinder from 'vue-tinder'
+
+Vue.use(VueTinder)
