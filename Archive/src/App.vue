88ed9aba441c4598bb244e1b831b701b55d489aa
ezhang7423 <ezhang7423@student.palomar.edu>
2020-01-11 20:27:00 -0800

    fix  description

--- Archive/src/App.vue
+++ Archive/src/App.vue
@@ -1,16 +1,44 @@
 <template>
-    <div id="app">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-        <router-view/>
-    </div>
+  <div id="app">
+    <link
+      href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
+      rel="stylesheet"
+    />
+    <router-view />
+  </div>
 </template>
 
 <style>
-    #app{
-        font-family: 'Open Sans', sans-serif;
+#app {
+  font-family: "Open Sans", sans-serif;
+}
+body {
+  margin: 0;
+}
+.auth_button {
+  margin-top: 20px;
+  background-color: rgb(79, 101, 241);
+  color: rgb(255, 255, 255);
+  font-size: 15px;
+  font-weight: 400;
+  height: 44px;
+  letter-spacing: 1px;
+  line-height: inherit;
+  text-transform: uppercase;
+  width: 100%;
+  border-radius: 4px;
+  padding: 9px 20px;
+  text-decoration: none;
+  border: none;
+}
 
-    }
-    body{
-        margin: 0
-    }
+.auth_button:hover {
+  background-color: rgb(19, 61, 170);
+  color: white !important;
+}
+
+.auth_button:focus {
+  background-color: rgb(19, 61, 170);
+  color: white !important;
+}
 </style>
--- Archive/src/main.js
+++ Archive/src/main.js
@@ -1,13 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import ElementUI from 'element-ui';
-import '@/theme/index.css'
-
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import ElementUI from "element-ui";
+import "@/theme/index.css";
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app');
+}).$mount("#app");
