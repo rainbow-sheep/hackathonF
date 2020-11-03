dc561cdbbc4069b165054ba0c996c57b35a7de58
ezhang7423 <ezhang7423@student.palomar.edu>
2020-11-02 22:41:25 -0800

    cleanup

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ Start the server
 ```bash
 npm run serve
 ```
+
+## Deployment
+
+Create a .env file fromm the webapp project in firebase. Look at the .env.sample for an example. If you don't have a firebase
+server yet, look at the backend instructions.
+
+Build the server
+
+```bash
+npm run build
+```
--- src/main.js
+++ src/main.js
@@ -4,21 +4,15 @@ import router from "./router";
 import ElementUI from "element-ui";
 import * as firebase from "firebase";
 import "@/theme/index.css";
-const configOptions = {
-  apiKey: "AIzaSyAVJYyi6Jc5pArnFBQUJqQ0reGb5Nxncvs",
-  authDomain: "carbon-vault-264802.firebaseapp.com",
-  databaseURL: "https://carbon-vault-264802.firebaseio.com",
-  projectId: "carbon-vault-264802",
-  storageBucket: "carbon-vault-264802.appspot.com",
-  messagingSenderId: "717670595891",
-  appId: "1:717670595891:web:d899acd0816fcffeea1246",
-  measurementId: "G-JELDNKJ3FB"
-};
+require("dotenv").config();
 
-firebase.initializeApp(configOptions);
+// Initialize Firebase
+
+firebase.initializeApp(JSON.parse(process.env.FIREBASECONFIG));
+
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
